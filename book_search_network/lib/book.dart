6c81895ea5d74998682f6a5ae6d4cfcfeaf72320
tintoll <belselios@gmail.com>
2021-03-09 15:24:59 +0900

    잭검색 앱 완료

---
+++ book_search_network/lib/book.dart
@@ -0,0 +1,9 @@
+class Book {
+  String title;
+  String author;
+  int price;
+  String status;
+  String imagePah;
+
+  Book(this.title, this.author, this.price, this.status, this.imagePah);
+}
--- book_search_network/lib/main.dart
+++ book_search_network/lib/main.dart
@@ -27,17 +27,88 @@ class BookSearchMain extends StatefulWidget {
 }
 
 class _BookSearchMainState extends State<BookSearchMain> {
-  var _api_call_result = '';
+  List books;
+  int page = 1;
+  ScrollController _scrollController;
+  TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    books = [];
+    _textEditingController = TextEditingController();
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      if (_scrollController.offset >=
+              _scrollController.position.maxScrollExtent &&
+          !_scrollController.position.outOfRange) {
+        page++;
+        getJsonData();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('책 정보')),
+      appBar: AppBar(
+        title: TextField(
+          style: TextStyle(color: Colors.white),
+          controller: _textEditingController,
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(hintText: '검색어를 입력하세요'),
+        ),
+      ),
       body: Container(
-        child: Text('$_api_call_result'),
+        child: books.length == 0
+            ? Text(
+                '데이터가 존재하지 않습니다.\n검색해주세요',
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
+              )
+            : ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Container(
+                      child: Row(
+                        children: <Widget>[
+                          Image.network(
+                            books[index]['thumbnail'],
+                            height: 100,
+                            width: 100,
+                            fit: BoxFit.contain,
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Container(
+                                width: MediaQuery.of(context).size.width - 150,
+                                child: Text(
+                                  books[index]['title'].toString(),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              Text(
+                                  '저자 : ${books[index]['authors'].toString()}'),
+                              Text(
+                                  '가격 : ${books[index]['sale_price'].toString()}'),
+                              Text(
+                                  '판매중 : ${books[index]['status'].toString()}'),
+                            ],
+                          )
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.start,
+                      ),
+                    ),
+                  );
+                },
+                itemCount: books.length,
+                controller: _scrollController,
+              ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          page = 1;
+          books.clear();
           getJsonData();
         },
         child: Icon(Icons.download_rounded),
@@ -46,8 +117,11 @@ class _BookSearchMainState extends State<BookSearchMain> {
   }
 
   Future<String> getJsonData() async {
-    // var url = 'https://dapi.kakao.com/v3/search/book?target=title&query=doit';
-    var uri =  Uri.https("dapi.kakao.com", "/v3/search/book", { "target" : "title", "query" : "doit" });
+    var uri = Uri.https("dapi.kakao.com", "/v3/search/book", {
+      "target": "title",
+      "query": "${_textEditingController.value.text}",
+      "page": "$page"
+    });
 
     var response = await http.get(uri,
         headers: {'Authorization': 'KakaoAK ec9d2531565bfb4456a818b349faba97'});
@@ -56,12 +130,12 @@ class _BookSearchMainState extends State<BookSearchMain> {
       var jsonResponse = convert.jsonDecode(response.body);
       print(jsonResponse.toString());
       setState(() {
-        _api_call_result = jsonResponse.toString();
+        var result = jsonResponse['documents'];
+        books.addAll(result);
       });
     } else {
-      setState(() {
-        _api_call_result = 'Request failed with status: ${response.statusCode}.';
-      });
+      print('Request failed with status: ${response.statusCode}.');
     }
+    return response.body;
   }
 }
