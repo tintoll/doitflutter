96e7fb699c488866403124077cb9f4114eec87ca
tintoll <belselios@gmail.com>
2021-03-15 05:40:50 +0900

    asset 파일 읽어서 내부저장소로 저장하는 부분 구현

--- internal_exam/lib/main.dart
+++ internal_exam/lib/main.dart
@@ -27,52 +27,102 @@ class FileApp extends StatefulWidget {
 }
 
 class _FileAppState extends State<FileApp> {
-
   int _count = 0;
+  List<String> itemList = [];
+  TextEditingController _textEditingController;
+
+  Future<List<String>> readListFile() async {
+    List<String> itemList = [];
+    var key = 'first';
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    bool firstCheck = pref.getBool(key);
+    var dir = await getApplicationDocumentsDirectory();
+    var fileExist = await File(dir.path + '/fruit.txt').exists();
+
+    if (firstCheck == null || firstCheck == false || fileExist == false) {
+      pref.setBool(key, true);
+      var file =
+          await DefaultAssetBundle.of(context).loadString("repo/fruit.txt");
+      File(dir.path + '/fruit.txt').writeAsString(file);
+
+      var array = file.split("/n");
+      for (var item in array) {
+        print(item);
+        itemList.add(item);
+      }
+      return itemList;
+    } else {
+      var file = await File(dir.path + '/fruit.txt').readAsString();
+      var array = file.split("/n");
+      for (var item in array) {
+        print(item);
+        itemList.add(item);
+      }
+      return itemList;
+    }
+  }
+
   @override
   void initState() {
     super.initState();
-    readCountFile();
+    _textEditingController = TextEditingController();
+    initData();
+  }
+
+  void initData() async {
+    var result = await readListFile();
+    setState(() {
+      itemList.addAll(result);
+    });
+  }
+
+  void writeFruit(String fruit) async {
+    var dir = await getApplicationDocumentsDirectory();
+    var file = await File(dir.path + '/fruit.txt').readAsString();
+    file = file + '\n' + fruit;
+    File(dir.path + '/fruit.txt').writeAsString(file);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title : Text('File Example')
-      ),
+      appBar: AppBar(title: Text('File Example')),
       body: Container(
         child: Center(
-          child: Text(
-            '$_count',
-            style: TextStyle(fontSize: 40),
+          child: Column(
+            children: [
+              TextField(
+                controller: _textEditingController,
+                keyboardType: TextInputType.text,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Center(
+                        child: Text(
+                          itemList[index],
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: itemList.length,
+                ),
+              )
+            ],
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          writeFruit(_textEditingController.value.text);
           setState(() {
-            _count++;
+            itemList.add(_textEditingController.value.text);
           });
-          writeCountFile(_count);
         },
         child: Icon(Icons.add),
-
       ),
     );
-  }
-
-  void writeCountFile(int count) async {
-    var dir = await getApplicationDocumentsDirectory();
-    File(dir.path+ '/count.txt').writeAsString(count.toString());
-  }
-
-  void readCountFile() async {
-    var dir = await getApplicationDocumentsDirectory();
-    var file = await File(dir.path + '/count.txt').readAsString();
-    print(file);
-    setState(() {
-      _count = int.parse(file);
-    });
   }
 }
