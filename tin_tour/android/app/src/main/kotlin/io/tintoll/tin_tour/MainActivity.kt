83ddf0e417263c32a9b702c1ef0e3d0370f85b63
tintoll <belselios@gmail.com>
2021-04-02 16:38:17 +0900

    모두의 여행- 로그인페이지

---
+++ tin_tour/android/app/src/main/kotlin/io/tintoll/tin_tour/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.tintoll.tin_tour
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tin_tour/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'login.dart';
+import 'sign_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '모두의 여행',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/' : (context) => LoginPage(),
+        'sign' : (context) => SignPage()
+      },
+    );
+  }
+}
